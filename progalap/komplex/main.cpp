6ba9729f9f0584f4d9ce575bda32f022707b04dc
David Koller <dkoller@geomant.com>
2020-01-05 19:54:13 +0100

    complex: added user input validation

--- progalap/komplex/main.cpp
+++ progalap/komplex/main.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
+#include<limits>
 
 using namespace std;
 
+const int minCountOfCities = 1;
 const int maxCountOfCities = 1000;
+
+const int minCountOfDays = 1;
 const int maxCountOfDays = 1000;
+
+const int minSampleValue = -50;
+const int maxSampleValue = 50;
+
 const bool verboseMode = false;
+const bool skipInputValidation = true;
 
 struct inputStruct
 {
@@ -19,34 +28,83 @@ struct outputStruct
 	int maxTempCities[maxCountOfCities];
 };
 
-void printVerbose(string text)
+void printVerbose(string text, bool endline=true)
 {
-	if (verboseMode){ cout << text << endl;}
+	if (verboseMode)
+	{
+		cout << text;
+		if (endline)
+		{
+			cout << endl;
+		}
+	}
 }
 
-void printVerbose(int text)
+int readNumber(int minValue, int maxValue, string repeatText = "")
 {
-	if (verboseMode){ cout << text << endl;}
+	int number;	
+	
+	if (skipInputValidation)
+	{
+		cin >> number;
+		return number;
+	}
+	
+	if (repeatText != ""){printVerbose(repeatText, false);}
+	while (true)
+	{
+		cin >> number;
+		
+		if (cin.fail())
+		{
+			printVerbose("Failed to read number from the screen, please try again!");
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(),'\n');
+			if (repeatText != ""){printVerbose(repeatText, false);}
+		}
+		else
+		{
+			if (number < minValue)
+			{
+				printVerbose("Error! value is too low, min. possible value: " + to_string(minValue));
+				if (repeatText != ""){printVerbose(repeatText, false);}
+			}
+			else
+			{
+				if (number > maxValue)
+				{
+					printVerbose("Error! value is too high, max. possible value: " + to_string(maxValue));	
+					if (repeatText != ""){printVerbose(repeatText, false);}					
+				}
+				else
+				{
+					break;
+				}
+			}
+		}
+	}
+	
+	return number;
 }
 
 
 inputStruct readInput()
 {
 	inputStruct input;
-	printVerbose("Please Type in the number of the cities the temp. samples have been made in:");
-	cin >> input.countOfCities;
+	
+	printVerbose("Please Type in the number of the cities the temp. samples have been made in: ", false);
+	input.countOfCities = readNumber(minCountOfCities, maxCountOfCities);
 
-	printVerbose("Please Type in the number of the days when temp. samples have been made:");
-	cin >> input.countOfDays;
+	printVerbose("Please Type in the number of the days when temp. samples have been made: ", false);
+	input.countOfDays = readNumber(minCountOfDays, maxCountOfDays);
 	
 	for (int i=0; i<input.countOfCities; i++)
 	{
-		printVerbose("Now it's time to read the " + i );
+		printVerbose("Now it's time to read the temperature samples for city #" + to_string(i+1));
 		for (int j=0; j<input.countOfDays; j++)
 		{
-			int number;
-			cin >> number;
-			input.samples[i][j] = (char)number;
+			string sampleInputText =  "City #" + to_string(i+1) + " Sample #" + to_string(j+1) + ": ";
+			input.samples[i][j] = (signed char)readNumber(minSampleValue, maxSampleValue, sampleInputText);
 		}
 	}
 	return input;
@@ -108,7 +166,11 @@ outputStruct getCitiesWithMaxTemp(inputStruct inputData, int maxTempValue)
 
 void printOutput(outputStruct outputData)
 {
+	printVerbose("The number of cities with the highest temperatures measured: ", false);
 	cout << outputData.maxTempCitiesCount;
+	if (verboseMode){cout << endl;}
+	
+	printVerbose("The list of the cities:", false);
 	for (int i=0; i<outputData.maxTempCitiesCount; i++)
 	{
 		cout << " " << outputData.maxTempCities[i]+1;
