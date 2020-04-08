9e925cd750c158f26ab0d82321658971986542df
David Koller <dkoller@geomant.com>
2020-04-08 20:19:58 +0200

    bead2

--- oop/bead2/participantEnumerator.cpp
+++ oop/bead2/participantEnumerator.cpp
@@ -1,7 +1,6 @@
 #include "participantEnumerator.h"
 #include <string>
 #include "raceEnumerator.h"
-#include <iostream>
 
 void ParticipantEnumerator::First()
 {
@@ -11,45 +10,22 @@ void ParticipantEnumerator::First()
 
 void ParticipantEnumerator::Next()
 {
-    std::cout << "ParticipantEnumerator::Next() invoked" << std::endl;
+    _end = _raceEnumerator.End();
 
-    if (_raceEnumerator.End())
-    {
-        if (_raceEnumerator.Current().participantName == _currentParticipantName)
-        {
-            _currentParticipantCarpsCounter = _currentParticipantCarpsCounter + 1;
-            std::cout << _currentParticipantName << " " << _currentParticipantCarpsCounter << std::endl;
-        }
-        else
-        {
-            _currentParticipantName = _raceEnumerator.Current().participantName;
-            _currentParticipantCarpsCounter = 0;
-            std::cout << _currentParticipantName << " " << _currentParticipantCarpsCounter << std::endl;
-            if (_raceEnumerator.Current().anyCarpCaught)
-            {
-                _currentParticipantCarpsCounter = _currentParticipantCarpsCounter + 1;
-                std::cout << _currentParticipantName << " " << _currentParticipantCarpsCounter << std::endl;
-            }
-        }
-
-        _end = true;
-    }
-    else
+    if (!_end)
     {
         _currentParticipantName = _raceEnumerator.Current().participantName;
         _currentParticipantCarpsCounter = 0;
-        std::cout << _currentParticipantName << " " << _currentParticipantCarpsCounter << std::endl;
 
         while (!_raceEnumerator.End() && _raceEnumerator.Current().participantName == _currentParticipantName)
         {
             if (_raceEnumerator.Current().anyCarpCaught)
             {
                 _currentParticipantCarpsCounter = _currentParticipantCarpsCounter + 1;
-                std::cout << _currentParticipantName << " " << _currentParticipantCarpsCounter << std::endl;
             }
             _raceEnumerator.Next();
         }
-    } 
+    }
 }
 
 bool ParticipantEnumerator::End()
