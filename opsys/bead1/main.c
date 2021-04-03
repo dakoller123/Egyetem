9d3b93684a78a1887cc51b618a6b6f5febddd2fd
Koller Dávid Dániel <dkoller@geomant.com>
2021-04-03 12:31:38 +0200

    .

--- opsys/bead1/main.c
+++ opsys/bead1/main.c
@@ -1,11 +1,109 @@
 #include <stdio.h>
+#include <string.h>
+ #include <stdbool.h>
+
+struct Record
+{
+
+//(Például: Vakci Áci, 1953, 36301234567, igen )
+    int Id;
+    char* Name;
+    int BirthYear;
+    long PhoneNumber;
+    bool Paid;
+};
+
+void createRecord()
+{
+
+}
+
+void readRecord()
+{
+
+}
+
+void updateRecord()
+{
+
+}
+
+void deleteRecord()
+{
+
+}
+
+void listRecords()
+{
+
+}
+
+void invalidArguments()
+{
+    printf("%s \n", "Invalid Arguments, try '.\vakcinacio H' to see the help");
+}
+
 int main(int argc, char *argv[]) {
-    if (argc > 1)
+    bool validArgs = false;
+
+    if ((argc == 2) && ((strcmp(argv[1], "H")) == 0))
     {
-        for (int i=0; i < argc-1; i++)
+        validArgs = true;
+        printf("%s\n", "Usage: specify the db filename as the first parameter, then either C,R,U,D,L for the options.");
+        printf("%s\n", "Example: ./vakcinacio ./db.txt C GipszJakab 1945 0036202536099 True");
+        printf("%s\n", "Example2: ./vakcinacio ./db.txt R 15");
+        printf("%s\n", "For additional help: Type H and either C,R,U,D,L to get the help for those commands.");
+        printf("%s\n", "Example: ./vakcinacio H C");
+    }
+
+    if (argc > 2)
+    {
+
+        char* fileName = argv[2];
+
+        if ((argc == 7) && (strcmp(argv[1], "C")) == 0)
         {
-            printf("%s \n", argv[i+1]);
+            //"bin fileName C Name BirthYear Phone Paid"
+            validArgs = true;
+            createRecord();
+        }
+
+        if ((argc == 4) && (strcmp(argv[1], "R")) == 0)
+        {
+            //"bin fileName R Id"
+            validArgs = true;
+            readRecord();
+        }
+
+        if ((argc == 9) && (strcmp(argv[1], "U")) == 0)
+        {
+            //"bin fileName U Id C Name BirthYear Phone Paid"
+            validArgs = true;
+            updateRecord();
+        }
+
+        if ((argc == 4) && (strcmp(argv[1], "D")) == 0)
+        {
+            //"bin fileName D Id"
+            validArgs = true;
+            deleteRecord();
+        }
+
+        if ((argc == 3) && (strcmp(argv[1], "L")) == 0)
+        {
+            //"bin fileName L"
+            validArgs = true;
+            listRecords();
         }
     }
 
+    if (!validArgs)
+    {
+        invalidArguments();
+    }
+
+
+
 }
+
+
